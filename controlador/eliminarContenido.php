537b3dd1928f2cdd6ce7e77051be1682c2dec555
alfredLazaro <alfredolazaro999@gmail.com>
2023-06-23 15:06:43 -0400

    se creo una modificacion de modificar contenido

--- controlador/eliminarContenido.php
+++ controlador/eliminarContenido.php
@@ -1,5 +1,29 @@
 <?php
 include_once "../modelo/model_user.php";
+$user = new User();
+
+if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+    if (isset($_POST['id']) && isset($_POST['nuevoTexto'])) {
+        $id = intval($_POST['id']);
+        $nuevoTexto = $_POST['nuevoTexto'];
+
+        $resultado = $user->modificarComment($id, $nuevoTexto);
+
+        if ($resultado) {
+            echo json_encode(['success' => true, 'message' => 'Comment modified successfully']);
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Failed to modify comment']);
+        }
+    } else {
+        echo json_encode(['success' => false, 'message' => 'Incomplete data']);
+    }
+    exit();
+} else {
+    header("Location: ../index.php");
+    exit();
+}
+
+/* include_once "../modelo/model_user.php";
 
 if (isset($_POST['id'])) {
     $user = new User();
@@ -19,4 +43,4 @@ if (isset($_POST['id'])) {
 } else {
     echo json_encode(['success' => false, 'message' => 'No ID provided']);
 }
-exit();
+exit(); --> */
